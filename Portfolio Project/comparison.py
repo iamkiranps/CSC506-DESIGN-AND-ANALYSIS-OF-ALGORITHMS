86f7803ff8f1b6e3d30dcee7772433a7e592fdf5
Kiran Ponappan Sreekumari <k.ponappansreekumar@farmersinsurance.com>
2024-03-08 11:45:20 -0800

    Module 8 PP

--- Portfolio Project/comparison.py
+++ Portfolio Project/comparison.py
@@ -10,39 +10,78 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the dataset from CSV
-df = pd.read_csv('Results/FullSize.csv')
+df = pd.read_csv('results/FullSize.csv')
 
 # Extract data for each operation
 insert_data = df[['Python Insert', 'Numpy Insert', 'Linked List Insert']]
 search_data = df[['Python Search', 'Numpy Search', 'Linked List Search']]
 delete_data = df[['Python Delete', 'Numpy Delete', 'Linked List Delete']]
 
-# Create a figure and subplots
-fig, axs = plt.subplots(3, 1, figsize=(10, 10))
+python_data = df[['Python Insert', 'Python Search', 'Python Delete']]
+numpy_data = df[['Numpy Insert', 'Numpy Search', 'Numpy Delete']]
+linkedlist_data = df[['Linked List Insert', 'Linked List Search', 'Linked List Delete']]
 
-# Plot insert operation comparison
-axs[0].plot(insert_data)
-axs[0].set_title('Insert Operation Comparison')
-axs[0].set_xlabel('Iterations')
-axs[0].set_ylabel('Time (nanoseconds)')
-axs[0].legend(insert_data.columns)
+def plot_1():
+  # Create a figure and subplots
+  fig, axs = plt.subplots(3, 1, figsize=(10, 10))
 
-# Plot search operation comparison
-axs[1].plot(search_data)
-axs[1].set_title('Search Operation Comparison')
-axs[1].set_xlabel('Iterations')
-axs[1].set_ylabel('Time (nanoseconds)')
-axs[1].legend(search_data.columns)
+  # Plot insert operation comparison
+  axs[0].plot(insert_data)
+  axs[0].set_title('Insert Operation Comparison')
+  axs[0].set_xlabel('Iterations')
+  axs[0].set_ylabel('Time (nanoseconds)')
+  axs[0].legend(insert_data.columns)
 
-# Plot delete operation comparison
-axs[2].plot(delete_data)
-axs[2].set_title('Delete Operation Comparison')
-axs[2].set_xlabel('Iterations')
-axs[2].set_ylabel('Time (nanoseconds)')
-axs[2].legend(delete_data.columns)
+  # Plot search operation comparison
+  axs[1].plot(search_data)
+  axs[1].set_title('Search Operation Comparison')
+  axs[1].set_xlabel('Iterations')
+  axs[1].set_ylabel('Time (nanoseconds)')
+  axs[1].legend(search_data.columns)
 
-# Adjust layout
-plt.tight_layout()
+  # Plot delete operation comparison
+  axs[2].plot(delete_data)
+  axs[2].set_title('Delete Operation Comparison')
+  axs[2].set_xlabel('Iterations')
+  axs[2].set_ylabel('Time (nanoseconds)')
+  axs[2].legend(delete_data.columns)
 
-# Show the plot
-plt.show()
+  # Adjust layout
+  plt.tight_layout()
+
+  # Show the plot
+  plt.show()
+
+def plot_2():
+  # Create a figure and subplots
+  fig, axs = plt.subplots(3, 1, figsize=(10, 10))
+
+  # Plot insert operation comparison
+  axs[0].plot(python_data)
+  axs[0].set_title('Python List Operation Comparison')
+  axs[0].set_xlabel('Iterations')
+  axs[0].set_ylabel('Time (nanoseconds)')
+  axs[0].legend(python_data.columns)
+
+  # Plot search operation comparison
+  axs[1].plot(numpy_data)
+  axs[1].set_title('Numpy Array Operation Comparison')
+  axs[1].set_xlabel('Iterations')
+  axs[1].set_ylabel('Time (nanoseconds)')
+  axs[1].legend(numpy_data.columns)
+
+  # Plot delete operation comparison
+  axs[2].plot(linkedlist_data)
+  axs[2].set_title('Linked List Operation Comparison')
+  axs[2].set_xlabel('Iterations')
+  axs[2].set_ylabel('Time (nanoseconds)')
+  axs[2].legend(linkedlist_data.columns)
+
+  # Adjust layout
+  plt.tight_layout()
+
+  # Show the plot
+  plt.show()
+
+if __name__ == '__main__':
+  plot_2()
